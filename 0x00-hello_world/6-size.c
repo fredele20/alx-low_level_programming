f54be9523541133722d824a8b503e1e3fbd342a1
fredele20 <victorisholaoladele@gmail.com>
2022-12-08 16:54:08 -0800

    task 6 implementation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long int));
+	printf(sizeof(long));
+	printf(sizeof(float));
+
+	return (0);
+
+}
