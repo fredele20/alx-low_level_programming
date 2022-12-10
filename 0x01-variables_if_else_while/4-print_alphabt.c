c7334052b2058c1c84efbac9e5e6db88c7edcec4
fredele20 <victorisholaoladele@gmail.com>
2022-12-09 19:18:58 -0800

    task 4 correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,10 +14,13 @@ int main(void)
 
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++) {
+
 		if (low == 'e' || low == 'q')
 			continue;
+
 		putchar(low);
+	}
 	putchar('\n');
 
 	return (0);
