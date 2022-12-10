8c255f9318632b67dcf6e6bea5698baac45e0530
fredele20 <victorisholaoladele@gmail.com>
2022-12-09 20:43:25 -0800

    task 8 implementation

---
+++ 0x01-variables_if_else_while/8-print_base_16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	int d;
+	char alph;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (alp = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
+	putchar('\n');
+
+	return (0);
+
+}
