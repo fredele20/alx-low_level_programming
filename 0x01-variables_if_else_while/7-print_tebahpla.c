02222c1716fcf243cb75baa9a823039d24ec2c03
fredele20 <victorisholaoladele@gmail.com>
2022-12-09 19:59:28 -0800

    task 7 implementation

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	char alp;
+
+	for (alp = 'z' alp >= 'a'; low--)
+		putchar(alp);
+	putchar('\n');
+
+	return (0);
+
+}
