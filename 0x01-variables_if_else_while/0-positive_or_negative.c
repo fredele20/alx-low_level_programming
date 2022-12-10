9bb4c13e795577ce9dec5cd05ddb6c3bbfc7a449
fredele20 <victorisholaoladele@gmail.com>
2022-12-09 18:14:06 -0800

    task 0 name correction error

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program assigns a random number
+ * to a variable each time it is executed
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		printf("%d, is zero\n", &n);
+	else if (n > 1)
+		printf("%d, is positive\n", &n);
+	else
+		printf("%d, is negative\n", &n);
+
+	return (0);
+
+}
