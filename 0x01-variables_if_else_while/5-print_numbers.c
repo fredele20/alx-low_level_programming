6ea25016e2ea9064b3cba499cd51717c84a1dd4f
fredele20 <victorisholaoladele@gmail.com>
2022-12-09 19:38:06 -0800

    task 5 implementation

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	while (int i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+
+}
