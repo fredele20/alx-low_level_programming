849eb5950be06c42528bc32f3fc0f80271b7fc59
fredele20 <victorisholaoladele@gmail.com>
2022-12-09 19:45:22 -0800

    task 6 implementation

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	for (int i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+
+}
