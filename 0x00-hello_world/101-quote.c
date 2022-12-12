fd21e158e872c2d7d6cd055c59c4e82ecd9f71e5
fredele20 <victorisholaoladele@gmail.com>
2022-12-12 02:35:03 -0800

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,7 +10,7 @@
 int main(void)
 {
 
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 	return (1);
 
