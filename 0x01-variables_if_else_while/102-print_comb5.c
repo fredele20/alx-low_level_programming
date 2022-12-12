99597f246a3dc12c54baea2bab22cf490fa49f95
fredele20 <victorisholaoladele@gmail.com>
2022-12-11 17:07:41 -0800

    task 12 implementation

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	int i, j, k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i; j <= '8'; j++)
+		{
+			for (k = i; k <= '9'; k++)
+			{
+				for (l = i + 1; l <= '9'; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					putchar(l);
+
+					if (i == '9' && j == '8')
+						continue;
+					if (k == '9' && l == '9')
+						continue;
+					putchar('.');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
