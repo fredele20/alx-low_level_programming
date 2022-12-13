5cee387176014d3b478548daedd5d79d07df742c
fredele20 <victorisholaoladele@gmail.com>
2022-12-13 03:48:00 -0800

    task 0 implementation

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always zero (Success)
+ *
+ * this function prints _putchar
+ *
+ */
+
+int main(void)
+{
+
+	printf("_putchar");
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file contains the descriptions of all the codes written inside all the other .c files in this directory
