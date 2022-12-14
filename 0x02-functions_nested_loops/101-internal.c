b97539b8b399726220e10bb8f212ea868dc2eebf
fredele20 <victorisholaoladele@gmail.com>
2022-12-14 01:45:40 -0800

    task 13 implementation

---
+++ 0x02-functions_nested_loops/101-internal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - a program that computes and prints the
+ * sum of all the multiples of 3 and 5 below 1024
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i = 1, sum;
+
+	for (i; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = i + sum;
+	}
+
+	printf("Sum: %d\n", sum);
+
+	return (0);
+
+}
