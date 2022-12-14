897fd6e32b5fafc5a43abeb944a3323ef34bc8ee
fredele20 <victorisholaoladele@gmail.com>
2022-12-13 16:44:48 -0800

    task 3 implementation

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase characters
+ *
+ * Return
+ *
+ */
+
+int _islower(int c)
+{
+
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
+
+}
