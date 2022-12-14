5b3cd05cae92a87a7cd774d473025c091ca6c7e9
fredele20 <victorisholaoladele@gmail.com>
2022-12-14 03:06:51 -0800

    another adjustment on output

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,15 +10,15 @@
 int main(void)
 {
 
-	int num1 = 1, num2 = 2, num3, i;
+	long int num1 = 1, num2 = 2, num3, i;
 
-	printf("%u, %u, ", num1, num2);
+	printf("%ld, %ld, ", num1, num2);
 
-	for (i = 2; i <= 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		num3 = num1 + num2;
 
-		printf("%u, ", num3);
+		printf("%ld, ", num3);
 
 		num1 = num2;
 		num2 = num3;
