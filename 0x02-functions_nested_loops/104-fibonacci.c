45ce713a56c43f78ede62a2e351ec2cf195c5e4b
fredele20 <victorisholaoladele@gmail.com>
2022-12-14 16:34:00 -0800

    task 16 correction

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,19 +11,17 @@
 int main(void)
 {
 
-	int num1 = 1, num2 = 2, i;
+	long int num1 = 1, num2 = 2, i, num3;
 
-	int num3;
-
-	printf("%d, %d, ", num1, num2);
+	printf("%lu, %lu, ", num1, num2);
 
 	for (i = 2; i < 98; i++)
 	{
 		num3 = num1 + num2;
+		printf("%lu", num3);
 
-		if (i != 98)
+		if (i != 97)
 			printf(", ");
-		printf("%u", num3);
 		num1 = num2;
 		num2 = num3;
 	}
