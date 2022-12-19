9a79bf931b43b5621f39ecba9b36d2d518268531
fredele20 <victorisholaoladele@gmail.com>
2022-12-19 15:32:44 -0800

    correcting task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_prev - this function prints a string in a reverse order
+ * print_rev - this function prints a string in a reverse order
  *
  * @s: a pointer parameter of type char
  *
  * Return: always 0 (Success)
  */
 
-void print_prev(char *s)
+void print_rev(char *s)
 {
 
 	int i = 0;
