ed3a4a560c38416d48f229fcfe27ebfb04da33f6
fredele20 <victorisholaoladele@gmail.com>
2022-12-19 07:22:34 -0800

    task 0 implementation

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** reset_to_98 - resets a value to 98 using pointers
+ *
+ * @n: pointer integer parameter
+ *
+ * Return: always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return;
+}
