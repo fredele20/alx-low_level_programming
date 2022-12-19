2072ae47393620386cf1b06de86f21fba36ce93d
fredele20 <victorisholaoladele@gmail.com>
2022-12-19 15:16:39 -0800

    task 4 implementation

---
+++ 0x05-pointers_arrays_strings/4-print_prev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_prev - this function prints a string in a reverse order
+ *
+ * @s: a pointer parameter of type char
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_prev(char *s)
+{
+
+	int i = 0;
+
+	while (s[i] - 1 != '\0')
+	{
+		_putchar(s[i]);
+
+		i++;
+	}
+
+	_putchar('\n');
+
+}
