58e6737a799ca1dad30e98032c6c0060a7bd8b33
fredele20 <victorisholaoladele@gmail.com>
2023-01-02 16:53:43 -0800

    task 0 implementation

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - this function fills the memory with constant bytes
+ * @s: location to fill
+ * @b: to fill location with
+ * @n: number of byte to fill
+ * Return: returns pointer to the filled location
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *start = s;
+
+	while (i--)
+	{
+		*s = b;
+		s++;
+	}
+	return (start);
+}
