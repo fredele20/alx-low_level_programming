195636393a4222978feecfde19eb0aeadf44f060
fredele20 <victorisholaoladele@gmail.com>
2023-01-02 18:13:25 -0800

    task 0 correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *start = s;
+	unsigned int i;
 
-	while (i--)
-	{
-		*s = b;
-		s++;
-	}
-	return (start);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
