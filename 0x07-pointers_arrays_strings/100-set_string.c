671b1939d3b66fb8d08eaf727c7a869b749c65c0
fredele20 <victorisholaoladele@gmail.com>
2023-01-02 18:26:49 -0800

    task 100 implementation

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets a pointer to another
+ * @s: input
+ * @to: input
+ * Return: always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
