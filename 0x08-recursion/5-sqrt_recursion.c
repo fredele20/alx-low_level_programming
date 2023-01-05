c66a2876ca6604d216097a732b08ac3f62b78ff5
fredele20 <victorisholaoladele@gmail.com>
2023-01-04 17:55:03 -0800

    task 5 correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (squareroot(n, (n + 1) / 2));
+		return (squareroot(1, n));
 }
 
 /**
@@ -23,10 +23,10 @@ int _sqrt_recursion(int n)
  */
 int squareroot(int n, int i)
 {
-	if (i < 1)
+	if (n > i)
 		return (-1);
-	else if (i * i == n)
+	else if (n * n == i)
 		return (i);
 	else
-		return (squareroot(n, i - 1));
+		return (squareroot(n + 1, i));
 }
