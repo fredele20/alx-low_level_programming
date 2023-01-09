94c5f932daa1d954c84a636f8e2f48017451c68a
fredele20 <victorisholaoladele@gmail.com>
2023-01-09 00:43:34 -0800

    task 1 implementation

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments passed into main
+ * @argv: The character to print
+ * @argc: The character to print
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv
+	printf("%d\n", argc - 1)
+	return (0)
+}
